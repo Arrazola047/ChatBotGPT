19f8c4ecbdc5b3f3fec377ca964d3a156ec7119e
Ramon Arrazola <arrazola047@gmail.com>
2023-05-03 23:24:18 -0600

    configuramos la biblioteca de openai

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import os
+import openai
 from dotenv import load_dotenv
 
+# Cargamos nuestra funcion env del modulo dotenv descargado en pip 
 load_dotenv()
 
+#Definimos nuestra API key 
 apikey = os.getenv('OPENAI_API_KEY')
+#Configuramos la biblioteca de openai
+openai.apikey = apikey
+
+#listamos los modelos disponibles para trabajar con la api de openAI y las almacenamos en la variable modelos
+modelos = openai.Model.list()
